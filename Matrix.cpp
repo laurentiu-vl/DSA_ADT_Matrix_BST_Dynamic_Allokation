4e1295cc28415238516c6b95d5144192948f7324
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-05-20 01:42:55 +0300

    modify()

--- Matrix.cpp
+++ Matrix.cpp
@@ -11,6 +11,9 @@ Matrix::Matrix(int nrLines, int nrCols) {
     nrOfCols = nrCols;
     nrOfRows = nrLines;
     root = nullptr;
+
+
+
 }
 
 
@@ -52,7 +55,36 @@ TElem Matrix::element(int i, int j) const {
 }
 
 TElem Matrix::modify(int i, int j, TElem e) {
-    //TODO - Implementation
+    if (i < 0 || j < 0 || i >= nrOfRows || j >= nrOfCols) {
+        throw runtime_error("Matrix::element(): Invalid input");
+    }
+
+    if (nrOfRows <= 0 || nrOfCols <= 0) {
+        throw runtime_error("Matrix::element(): Invalid input");
+    }
+
+    BSTNode *parent = nullptr;
+    BSTNode *current = root;
+    TElem oldInfo;
+    bool found = false;
+
+    while (current != nullptr) {
+        if (current->row == i && current->col == j) {
+            oldInfo = current->info;
+            found = true;
+            current->info = e;
+        }
+        if (found == true) {
+            return oldInfo;
+        }
+        if (i < current->row) { //check first the line
+            current = current->leftC;
+        }
+        if (i == current->row && j < current->col) { //if line=i, check the column for traver to leftC
+            current = current->leftC;
+        }
+        else current = current->rightC; //if i/j bigger
+    }
     return NULL_TELEM;
 }
 
