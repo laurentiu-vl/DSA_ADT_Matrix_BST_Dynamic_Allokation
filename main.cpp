433052a69fd7c5ffb445ffae132aae04581ca31e
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-05-26 17:49:16 +0300

    main

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 using namespace std;
 
 int main() {
-    testAll();
-    testAllExtended();
+    //testAll();
+
+    testAllLaurentiu();
+    //testAllExtended();
     cout << "Test End" << endl;
     system("pause");
 }
