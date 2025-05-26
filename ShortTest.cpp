947074a2dfac00ca50fccd1d4a118be972c013bf
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-05-26 17:48:55 +0300

    Laurentiu short test

--- ShortTest.cpp
+++ ShortTest.cpp
@@ -6,12 +6,35 @@
 
 using namespace std;
 
-void testAll() {
+// void testAll() {
+//     Matrix m(4, 4);
+//     assert(m.nrLines() == 4);
+//     assert(m.nrColumns() == 4);
+//     m.modify(1, 1, 5);
+//     assert(m.element(1, 1) == 5);
+//     m.modify(1, 1, 6);
+//     assert(m.element(1, 2) == NULL_TELEM);
+// }
+
+void testAllLaurentiu() {
     Matrix m(4, 4);
     assert(m.nrLines() == 4);
     assert(m.nrColumns() == 4);
-    m.modify(1, 1, 5);
-    assert(m.element(1, 1) == 5);
-    m.modify(1, 1, 6);
+
     assert(m.element(1, 2) == NULL_TELEM);
+
+    m.modify(1, 1, 4);
+    assert(m.element(1, 1) == 4);
+
+    m.modify(1, 2, 7);
+    assert(m.element(1, 2) == 7);
+
+    m.modify(2, 2, 10);
+    assert(m.element(2, 2) == 10);
+
+    m.modify(1, 1, 0);
+
+    m.modify(1, 2, 0);
+
+    m.modify(2, 2, 231);
 }
--- ShortTest.h
+++ ShortTest.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void testAll();
+
+void testAllLaurentiu();
