31107d17654d3f285bc0c2b24c8a9bf57ec96ebf
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-05-28 00:18:18 +0300

    all all tests

--- ShortTest.cpp
+++ ShortTest.cpp
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-// void testAll() {
-//     Matrix m(4, 4);
-//     assert(m.nrLines() == 4);
-//     assert(m.nrColumns() == 4);
-//     m.modify(1, 1, 5);
-//     assert(m.element(1, 1) == 5);
-//     m.modify(1, 1, 6);
-//     assert(m.element(1, 2) == NULL_TELEM);
-// }
+void testAll() {
+    Matrix m(4, 4);
+    assert(m.nrLines() == 4);
+    assert(m.nrColumns() == 4);
+    m.modify(1, 1, 5);
+    assert(m.element(1, 1) == 5);
+    m.modify(1, 1, 6);
+    assert(m.element(1, 2) == NULL_TELEM);
+}
 
 void testAllLaurentiu() {
     Matrix m(4, 4);
